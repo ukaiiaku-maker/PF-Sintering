0b252da41ff65e95e318d0a73784dd6f3d3fea89
ukaiiaku-maker <ukaiiaku@gmail.com>
2026-08-06 21:35:20 -0700

    refactor: export MATLAB-parity sintering runner

--- pf_sintering/__init__.py
+++ pf_sintering/__init__.py
@@ -1,6 +1,6 @@
 """Phase-field sintering model."""
 
 from .model import ModelConfig
-from .runner import SinteringModel
+from .parity_runner import SinteringModel
 
 __all__ = ["ModelConfig", "SinteringModel"]
